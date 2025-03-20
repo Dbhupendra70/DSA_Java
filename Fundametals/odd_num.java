a633f0dceaeec703aa6909ce456d79e7e9a5177b
Dbhupendra70 <143151701+Dbhupendra70@users.noreply.github.com>
2025-03-20 23:52:24 +0530

    1st assignment

---
+++ Fundametals/odd_num.java
@@ -0,0 +1,10 @@
+class odd_num{
+    public static void main(String [] args ) {
+        int max_limit = 100 ;
+        for ( int i  = 1 ; i <= max_limit ; i++ ){
+            if( i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
